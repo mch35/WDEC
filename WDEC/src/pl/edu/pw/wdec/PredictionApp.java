7d7019eaa4b8f6b28a2cdeb7636bbf3a0985d619
michal <michalchilczuk@gmail.com>
2014-12-30 18:05:29 +0100

    main app class

---
+++ WDEC/src/pl/edu/pw/wdec/PredictionApp.java
@@ -0,0 +1,16 @@
+package pl.edu.pw.wdec;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PredictionApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
